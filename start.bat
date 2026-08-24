ba4b4d9e4ed3e9ce25d4ff678f95714c72064b52
qq1473450426 <qq1473450426@gmail.com>
2026-08-25 01:25:43 +0800

    chore: remove duplicate launcher

--- start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-if not exist .venv python -m venv .venv
-call .venv\Scripts\activate
-python -m pip install --upgrade pip
-pip install -r requirements.txt
-python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
-pause
