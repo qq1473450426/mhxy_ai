5a7cf798872fb710dc74a035259db77e6cd27bf1
qq1473450426 <qq1473450426@gmail.com>
2026-08-25 02:38:26 +0800

    Delete tests directory

--- tests/test_local_reasoning.py
+++
@@ -1,9 +0,0 @@
-from app.services.ai import LocalReasoningEngine
-
-def test_reconnect_priority():
-    d=LocalReasoningEngine().decide('A01',{'state':'RUNNING','connected':False,'battle_detected':False,'dialog_detected':False,'target_found':False,'task_done':False,'confidence':.1},{'task_name':'test'})
-    assert d.action=='RECONNECT'
-
-def test_task_complete():
-    d=LocalReasoningEngine().decide('A01',{'state':'RUNNING','connected':True,'battle_detected':False,'dialog_detected':False,'target_found':False,'task_done':True,'confidence':.9},{'task_name':'test'})
-    assert d.action=='TASK_COMPLETE'
