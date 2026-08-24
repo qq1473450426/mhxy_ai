e558a704cc632af340d8e71d7e684373370257dd
qq1473450426 <qq1473450426@gmail.com>
2026-08-25 00:49:05 +0800

    refactor: start Django dashboard

--- start.bat
+++ start.bat
@@ -1,7 +1,8 @@
 @echo off
-cd /d %~dp0
 if not exist .venv python -m venv .venv
-call .venv\Scripts\activate.bat
-python -m pip install -r requirements.txt
-python run.py
+call .venv\Scripts\activate
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+python manage.py migrate
+python manage.py runserver 0.0.0.0:8000
 pause
