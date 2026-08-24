7d9bf54f18071d3b81d90cfbae4ef1a17f40bb1c
qq1473450426 <qq1473450426@gmail.com>
2026-08-25 01:26:49 +0800

    chore: remove obsolete service layer

--- app/services/scheduler.py
+++
@@ -1,5 +0,0 @@
-class Scheduler:
-    def __init__(self):self.workers={}
-    def register(self,w):self.workers[w.account_id]=w
-    def idle_replacement(self,exclude):
-        c=[w for aid,w in self.workers.items() if aid!=exclude and w.is_available_for_replacement()];c.sort(key=lambda x:x.priority,reverse=True);return c[0] if c else None
