418afad810c76c6f157a13367ae398928af5a1ac
qq1473450426 <qq1473450426@gmail.com>
2026-08-25 00:49:01 +0800

    refactor: run Django server by default

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import uvicorn
-
-if __name__ == "__main__":
-    uvicorn.run("app.api.main:app", host="127.0.0.1", port=8000)
+import os,sys
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','config.settings')
+from django.core.management import execute_from_command_line
+execute_from_command_line([sys.argv[0],'runserver','127.0.0.1:8000'])
