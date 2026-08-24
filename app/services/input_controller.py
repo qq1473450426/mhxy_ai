c151c10e345906ecb3cc17ee58f5e73d462452dd
qq1473450426 <qq1473450426@gmail.com>
2026-08-25 01:26:33 +0800

    chore: remove obsolete service layer

--- app/services/input_controller.py
+++
@@ -1,9 +0,0 @@
-import time,pyautogui
-class InputController:
-    def __init__(self,dry_run=True):self.dry_run=dry_run
-    def move_click(self,x,y):
-        if self.dry_run:return {'ok':True,'dry_run':True,'action':'click','x':x,'y':y}
-        pyautogui.moveTo(x,y,duration=.08);pyautogui.click();return {'ok':True}
-    def press(self,key):
-        if self.dry_run:return {'ok':True,'dry_run':True,'action':'press','key':key}
-        pyautogui.press(key);time.sleep(.05);return {'ok':True}
