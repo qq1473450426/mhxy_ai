3b9fefc7129116f40c42b90ef84bfa01744d4db9
qq1473450426 <qq1473450426@gmail.com>
2026-08-25 01:25:39 +0800

    chore: remove obsolete FastAPI/Uvicorn launcher

--- run.py
+++
@@ -1,4 +0,0 @@
-import os,sys
-os.environ.setdefault('DJANGO_SETTINGS_MODULE','config.settings')
-from django.core.management import execute_from_command_line
-execute_from_command_line([sys.argv[0],'runserver','127.0.0.1:8000'])
